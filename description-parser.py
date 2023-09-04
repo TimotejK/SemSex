3038459a5e7e6d8ab8a944e432481b780e4a447f
Timotej Knez <timotej.knez@fri.uni-lj.si>
2023-09-04 13:19:16 +0200

    Read and write the ontology into json

--- description-parser.py
+++ description-parser.py
@@ -1,13 +1,16 @@
 import re
+import json
 from rdflib import Graph, URIRef, BNode, Literal, Namespace, RDF, RDFS
 
+
 class Concept:
-    def __init__(self, line):
-        concept_test = re.compile(r"^((?:\d+\.)+) (.*)$")
-        result = concept_test.match(line)
-        index = result.group(1)
-        self.index = tuple([int(s) for s in index.split(".") if s.isnumeric()])
-        self.name = result.group(2)
+    def __init__(self, line=None):
+        if line is not None:
+            concept_test = re.compile(r"^((?:\d+\.)+) (.*)$")
+            result = concept_test.match(line)
+            index = result.group(1)
+            self.index = tuple([int(s) for s in index.split(".") if s.isnumeric()])
+            self.name = result.group(2)
         self.children = []
         self.links = []
         self.relations = {}
@@ -21,6 +24,38 @@ class Concept:
 
     def __str__(self):
         return str(self.index) + " " + self.name
+
+    def to_dict(self):
+        dict = vars(self).copy()
+        dict['children'] = []
+        for child in self.children:
+            dict['children'].append(child.index)
+        if self.parent is not None:
+            dict['parent'] = self.parent.index
+        return dict
+
+    def from_dict(self, dictionary):
+        self.__dict__.update(dictionary)
+        self.index = tuple(self.index)
+        if self.parent is not None:
+            self.parent = tuple(self.parent)
+
+    def replace_references(self, concepts):
+        if self.parent is not None:
+            self.parent = concepts[tuple(self.parent)]
+
+        for i in range(len(self.children)):
+            self.children[i] = concepts[tuple(self.children[i])]
+
+def concepts_from_json(lists):
+    concepts={}
+    for l in lists:
+        concept = Concept()
+        concept.from_dict(l)
+        concepts[concept.index] = concept
+    for ind in concepts:
+        concepts[ind].replace_references(concepts)
+    return concepts
 
 def parse_descriptions(text):
     concepts = {}
@@ -59,11 +94,24 @@ def parse_descriptions(text):
     for index in concepts:
         concepts[index].add_parent(concepts)
 
+    # save to json
+    entities_list = []
+    for index in concepts:
+        concept = concepts[index]
+        entities_list.append(concept.to_dict())
+    s = json.dumps(entities_list)
+    with open("ontologija.json", "w") as outfile:
+        outfile.write(s)
+    # load from json
+    con = concepts_from_json(json.loads(s))
+
     build_knowledge_graph(concepts)
+
 
 def build_knowledge_graph(concepts):
     def untuple(t):
         return ".".join([str(x) for x in t])
+
     g = Graph()
     concepts_ns = Namespace("http://example.org/concepts/")
     relations_ns = Namespace("http://example.org/relations/")
@@ -86,6 +134,7 @@ def build_knowledge_graph(concepts):
     v = g.serialize(format="turtle")
     with open('test_ontology.ttl', 'w') as f:
         f.write(v)
+
 
 if __name__ == '__main__':
     text_file = open("test-descriptions.txt", "r")
